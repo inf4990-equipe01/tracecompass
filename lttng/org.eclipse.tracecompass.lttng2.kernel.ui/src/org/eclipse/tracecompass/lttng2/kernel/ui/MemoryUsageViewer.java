c89e3dbe9d7019187759c8aba2ef07d8f2350163
Samuel Gagnon <samuel.gagnon92@gmail.com>
2016-01-21 21:18:21 -0500

    nettoyage du code

--- lttng/org.eclipse.tracecompass.lttng2.kernel.ui/src/org/eclipse/tracecompass/lttng2/kernel/ui/MemoryUsageViewer.java
+++ lttng/org.eclipse.tracecompass.lttng2.kernel.ui/src/org/eclipse/tracecompass/lttng2/kernel/ui/MemoryUsageViewer.java
@@ -27,8 +27,6 @@ public class MemoryUsageViewer extends TmfCommonXLineChartViewer {
 
     private TmfStateSystemAnalysisModule fModule = null;
 
-    private static final int BYTES_TO_KB = 1024;
-
     public MemoryUsageViewer(Composite parent) {
         super(parent, "Kernel Memory Usage", "Time", "Kb");
     }
@@ -43,68 +41,8 @@ public class MemoryUsageViewer extends TmfCommonXLineChartViewer {
                 System.out.println("fModule KernelMem non trouve");
                 return;
             }
-            //ss = TmfStateSystemAnalysisModule.getStateSystem(trace, KernelMemoryAnalysisModule.ID);
             fModule.schedule();
 
-//            //TEST----------------------
-//            TmfEventRequest req = new TmfEventRequest(TmfEvent.class,
-//                    TmfTimeRange.ETERNITY, 0, ITmfEventRequest.ALL_DATA,
-//                    ITmfEventRequest.ExecutionType.BACKGROUND) {
-//
-//                double mem = 0;
-//                ArrayList<Double> xValues = new ArrayList<>();
-//                ArrayList<Double> yValues = new ArrayList<>();
-//
-//                @Override
-//                public void handleData(ITmfEvent data) {
-//                    super.handleData(data);
-//                    String name = data.getName();
-//                    if (name.equals("kmem_mm_page_alloc") ) {
-//                        xValues.add((double) data.getTimestamp().getValue());
-//                        mem += 4096;
-//                        yValues.add(mem);
-//                    } else if (name.equals("kmem_mm_page_free")) {
-//                        xValues.add((double) data.getTimestamp().getValue());
-//                        mem -= 4096;
-//                        yValues.add(mem);
-//                    }
-//                }
-//
-//                @Override
-//                public void handleSuccess() {
-//                    super.handleSuccess();
-//
-//
-//                    xVal = toArray(xValues);
-//                    yVal = toArray(yValues);
-//
-//                    // J'ai aucune idée pourquoi je fais ça...
-//                    Display.getDefault().asyncExec(new Runnable() {
-//                        @Override
-//                        public void run() {
-//                            setXAxis(xVal);
-//                            setSeries("System usage", yVal);
-//                            updateDisplay();
-//                        }
-//                    });
-//                }
-//
-//                @Override
-//                public void handleFailure() {
-//                    super.handleFailure();
-//                }
-//
-//                private double[] toArray(List<Double> list) {
-//                    double[] d = new double[list.size()];
-//                    for (int i = 0; i < list.size(); ++i) {
-//                        d[i] = list.get(i);
-//                    }
-//
-//                    return d;
-//                }
-//            };
-//            trace.sendRequest(req);
-//            //TEST----------------------
         }
     }
 
@@ -125,7 +63,6 @@ public class MemoryUsageViewer extends TmfCommonXLineChartViewer {
 
         ss.waitUntilBuilt();
 
-
         double yvalue = 0.0;
         double[] values = new double[xvalues.length];
         for (int i = 0; i < xvalues.length; i++) {
@@ -137,7 +74,7 @@ public class MemoryUsageViewer extends TmfCommonXLineChartViewer {
             Integer testQuark;
             try {
                 testQuark = ss.getQuarkAbsolute("TestKernelMemory");
-                yvalue = ss.querySingleState((long) x + this.getTimeOffset(), testQuark.intValue()).getStateValue().unboxLong() / BYTES_TO_KB;
+                yvalue = ss.querySingleState((long) x + this.getTimeOffset(), testQuark.intValue()).getStateValue().unboxLong();
                 values[i] = yvalue;
             } catch (AttributeNotFoundException e) {
                 // TODO Auto-generated catch block
@@ -149,22 +86,6 @@ public class MemoryUsageViewer extends TmfCommonXLineChartViewer {
         }
         setSeries("Kernel mem usage", values);
         updateDisplay();
-
-
-//        System.out.println("updateData");
-//
-//        double[] xvalues = getXAxis(start, end, nb);
-//        setXAxis(xvalues);
-//
-//        double [] testY = new double[xvalues.length];
-//        System.out.println(testY.length);
-//        for(int i=0; i<testY.length; i++) {
-//            testY[i] = i;
-//        }
-//
-//        setSeries("Kernel mem usage", testY);
-//        updateDisplay();
-
     }
 
 }
